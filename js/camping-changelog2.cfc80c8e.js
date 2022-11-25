b52976dec5c85fa10135390415d7cda056be21a4
CeciliaBot <giovalin.psn@gmail.com>
2022-11-25 01:32:30 +0100

    Add AESPA base data + tier list maker improvmesnts
    * Add new emotes
    * Fix emotes in tier row label
    * Add resize labels

--- js/camping-changelog2.cfc80c8e.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self["webpackChunkv3"]=self["webpackChunkv3"]||[]).push([[296],{8370:function(e){e.exports=JSON.parse('[[0,"27/11/2022",[["new","This is a workin in progress.<br>Webpage URL may change in the final version."]]]]')}}]);
---
+++ js/locales/ja-artifacts-json.bbaca4ac.js
@@ -0,0 +1,1 @@
+"use strict";(self["webpackChunkv3"]=self["webpackChunkv3"]||[]).push([[534],{2885:function(e){e.exports={}}}]);
