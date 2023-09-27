1dc83aed202da2149d32ff6e92a7c1ede335c7dc
CeciliaBot <giovalin.psn@gmail.com>
2023-09-27 02:48:28 +0200

    Update ongoing-events.js

--- data/api/ongoing-events.js
+++ data/api/ongoing-events.js
@@ -91,7 +91,7 @@ export default async function handler(req, res) {
                 return {
                     type: 'dir',
                     name: file,
-                    children: mapAllFiles(file)
+                    children: mapAllFiles(entry + file + '/')
                 }
             }
         })
